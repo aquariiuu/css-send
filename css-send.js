17b061ebd3099471e197269145f6e7e8ef597153
aquariiuu <120671284+aquariiuu@users.noreply.github.com>
2023-12-05 13:47:29 -0800

    fixed scroll-height
    also changed variable from `r` to `cssSend.root`

--- css-send.js
+++ css-send.js
@@ -1,18 +1,17 @@
-var r = document.querySelector(':root');
-r.style.setProperty('--scroll', 0);
-r.style.setProperty('--scroll-height', window.scrollHeight );
-r.style.setProperty('--scroll-percent', 0 );
+var cssSend.root = document.querySelector(':root');
+cssSend.root.style.setProperty('--scroll', 0);
+cssSend.root.style.setProperty('--scroll-height', (document.documentElement.scrollHeight-window.innerHeight))
+cssSend.root.style.setProperty('--scroll-percent', 0 );
 
 document.addEventListener("scroll", (event) => {  
-  r.style.setProperty('--scroll', window.scrollY);
-  r.style.setProperty('--scroll-percent', window.scrollY/window.scrollHeight * 100 );
+  cssSend.style.setProperty('--scroll', window.scrollY);
+  cssSend.style.setProperty('--scroll-percent', window.scrollY/(document.documentElement.scrollHeight-window.innerHeight) * 100 );
 })
 document.addEventListener("click", (event) => {   
-  console.log(event.clientX, event.clientY)
-  r.style.setProperty('--lastClickX', event.clientX);
-  r.style.setProperty('--lastClickY', event.clientY);
+  cssSend.style.setProperty('--lastClickX', event.clientX);
+  cssSend.style.setProperty('--lastClickY', event.clientY);  
 })
 document.addEventListener("mousemove", (event) => {  
-  r.style.setProperty('--lastMouseX', event.clientX);
-  r.style.setProperty('--lastMouseY', event.clientY);
+  cssSend.style.setProperty('--lastMouseX', event.clientX);
+  cssSend.style.setProperty('--lastMouseY', event.clientY);
 })
