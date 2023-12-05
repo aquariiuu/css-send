e0ecd1a2d31f3441fe6e09aefcc0d1d063a23f7c
aquariiuu <120671284+aquariiuu@users.noreply.github.com>
2023-12-05 13:53:01 -0800

    Update css-send.js
    fixed css-send var

--- css-send.js
+++ css-send.js
@@ -1,17 +1,17 @@
-var cssSend.root = document.querySelector(':root');
+var cssSend = {"root": document.querySelector(':root')}
 cssSend.root.style.setProperty('--scroll', 0);
 cssSend.root.style.setProperty('--scroll-height', (document.documentElement.scrollHeight-window.innerHeight))
 cssSend.root.style.setProperty('--scroll-percent', 0 );
 
 document.addEventListener("scroll", (event) => {  
-  cssSend.style.setProperty('--scroll', window.scrollY);
-  cssSend.style.setProperty('--scroll-percent', window.scrollY/(document.documentElement.scrollHeight-window.innerHeight) * 100 );
+  cssSend.root.style.setProperty('--scroll', window.scrollY);
+  cssSend.root.style.setProperty('--scroll-percent', window.scrollY/(document.documentElement.scrollHeight-window.innerHeight) * 100 );
 })
 document.addEventListener("click", (event) => {   
-  cssSend.style.setProperty('--lastClickX', event.clientX);
-  cssSend.style.setProperty('--lastClickY', event.clientY);  
+  cssSend.root.style.setProperty('--lastClickX', event.clientX);
+  cssSend.root.style.setProperty('--lastClickY', event.clientY);  
 })
 document.addEventListener("mousemove", (event) => {  
-  cssSend.style.setProperty('--lastMouseX', event.clientX);
-  cssSend.style.setProperty('--lastMouseY', event.clientY);
+  cssSend.root.style.setProperty('--lastMouseX', event.clientX);
+  cssSend.root.style.setProperty('--lastMouseY', event.clientY);
 })
