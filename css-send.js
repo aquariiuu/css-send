2dbe236d69edd6a4db69751f4a5e623b561037e5
aquariiuu <120671284+aquariiuu@users.noreply.github.com>
2023-12-06 09:08:17 -0800

    Update css-send.js

--- css-send.js
+++ css-send.js
@@ -3,15 +3,15 @@ cssSend.root.style.setProperty('--scroll', 0);
 cssSend.root.style.setProperty('--scroll-height', (document.documentElement.scrollHeight-window.innerHeight))
 cssSend.root.style.setProperty('--scroll-percent', 0 );
 
-document.addEventListener("scroll", (event) => {  
+window.addEventListener("scroll", (event) => {  
   cssSend.root.style.setProperty('--scroll', window.scrollY);
-  cssSend.root.style.setProperty('--scroll-percent', window.scrollY/(document.documentElement.scrollHeight-window.innerHeight) * 100 );
+  cssSend.root.style.setProperty('--scroll-percent', window.scrollY/(document.documentElement.scrollHeight-window.innerHeight) * 100 ); 
 })
-document.addEventListener("click", (event) => {   
+window.addEventListener("click", (event) => {   
   cssSend.root.style.setProperty('--lastClickX', event.clientX);
   cssSend.root.style.setProperty('--lastClickY', event.clientY);  
 })
-document.addEventListener("mousemove", (event) => {  
+window.addEventListener("mousemove", (event) => {  
   cssSend.root.style.setProperty('--lastMouseX', event.clientX);
   cssSend.root.style.setProperty('--lastMouseY', event.clientY);
 })
